e179e0341cf2875698de9b0b531e3108b65892d5
ly980923 <2826013269@qq.com>
2020-08-28 19:45:34 +0800

    登录成功

---
+++ js/axios.defaults.js
@@ -0,0 +1,40 @@
+//对axios进行二次封装
+axios.defaults.baseURL = "http://127.0.0.1:8888"  //配置请求的基本路径
+//数据以表单的形式扔给服务器
+axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+
+//还是以表单的形式扔给服务器，数据格式：name=z3$age=4
+axios.defaults.transformRequest = function(data){
+    if(!data)  return data
+    let result = ``
+    for(let attr in data){
+        if(!data.hasOwnProperty(attr)) break
+        result += `&${attr}=${data[attr]}`
+    }
+    return result.substring(1)
+}
+//配置请求拦截器
+axios.interceptors.request.use(config=>{
+    return config
+})
+//配置响应拦截器
+axios.interceptors.response.use(response=>{
+    return response.data
+},reason=>{
+    //如果路径出错了，通过会返回404  还有一些其他问题....
+    // console.log("----------");
+    // console.dir(reason);
+    // console.log("----------");
+    if(reason.response){
+        switch(String(reason.response.status)){
+            case "404":
+                alert("当前请求的地址不存在！")
+                break
+            default:
+                break
+        }
+    }
+    //  直接创建出一个失败的promise
+    return Promise.reject(reason)
+
+})
